fea34baab6a0a7938f52204cb36492310db5961b
Jacob Mossberg <jacob.c.mossberg@gmail.com>
2014-03-15 21:22:57 +0100

    Changed to Eclipse CDT, added gcc install

--- scripts/install_tddcourse.sh
+++ scripts/install_tddcourse.sh
@@ -2,15 +2,20 @@
 echo Installing TDD Course by jacob.c.mossberg@gmail.com
 echo Install Eclipse
 echo ===============
-sudo apt-get install eclipse
+sudo apt-get install --quiet --assume-yes eclipse-cdt
+
+echo Install gcc
+echo ===========
+sudo apt-get install --quiet --assume-yes gcc
+sudo apt-get install --quiet --assume-yes g++
 
 echo Download Google Test/Google Mock
 echo ================================
 mkdir ~/google
 wget https://googlemock.googlecode.com/files/gmock-1.7.0.zip
 cd ~/google
-unzip gmock-1.7.0.zip
-rm -f gmock-1.7.0.zip
+unzip ~/google/gmock-1.7.0.zip -d ~/google
+rm -f ~/google/gmock-1.7.0.zip
 
 #echo Install Apache2
 #echo Install Jenkins
