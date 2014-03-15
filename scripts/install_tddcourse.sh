3591dbeb2782fb9f60b9428eb3fa36d53efdb9c7
Jacob Mossberg <jacob.c.mossberg@gmail.com>
2014-03-15 21:32:13 +0100

    Use $HOME instead of ~

--- scripts/install_tddcourse.sh
+++ scripts/install_tddcourse.sh
@@ -11,10 +11,10 @@ sudo apt-get install --quiet --assume-yes g++
 
 echo Download Google Test/Google Mock
 echo ================================
-mkdir -p ~/google
+mkdir -p $HOME/google
 wget --directory-prefix=~/google -q https://googlemock.googlecode.com/files/gmock-1.7.0.zip
-unzip ~/google/gmock-1.7.0.zip -d ~/google
-rm -f ~/google/gmock-1.7.0.zip
+unzip $HOME/google/gmock-1.7.0.zip -d ~/google
+rm -f $HOME/google/gmock-1.7.0.zip
 
 #echo Install Apache2
 #echo Install Jenkins
