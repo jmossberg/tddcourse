f096e2484b5498429bf77f0168dc344c37740442
tdd1 <tdd1@tdd1-VirtualBox>
2014-04-08 21:16:41 +0200

    Implemented new test "ConcatenateWordsUsingDynamicWordDelimiter"

--- reference/WordsrepApp/test/WordsToLineClassTest.cpp
+++ reference/WordsrepApp/test/WordsToLineClassTest.cpp
@@ -23,3 +23,23 @@ TEST_F(WordsToLineClassTest, ConcatenateThreeWordsIntoLine)
   //Verify
   ASSERT_EQ("table rocket moon", line);
 }
+
+TEST_F(WordsToLineClassTest, ConcatenateWordsUsingDynamicWordDelimiter)
+{
+	  //Setup
+	  char wordDelimiter = ';';
+	  WordsToLineClass wordsToLineClass(wordDelimiter);
+
+	  std::vector<std::string> words;
+	  words.push_back("table");
+	  words.push_back("rocket");
+	  words.push_back("moon");
+
+	  std::string line;
+
+	  //Exercise
+	  line = wordsToLineClass.concatenateWords(words);
+
+	  //Verify
+	  ASSERT_EQ("table;rocket;moon", line);
+}
