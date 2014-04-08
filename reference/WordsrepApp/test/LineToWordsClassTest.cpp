db674a64c9d28c9afad749aa461917224f25147e
tdd1 <tdd1@tdd1-VirtualBox>
2014-04-08 20:58:35 +0200

    Added test "CanSplitLineUsingDynamicWordDelimiter"

--- reference/WordsrepApp/test/LineToWordsClassTest.cpp
+++ reference/WordsrepApp/test/LineToWordsClassTest.cpp
@@ -20,3 +20,20 @@ TEST_F(LineToWordsClassTest, CanSplitLineWithTwoWords)
   ASSERT_EQ("street", words[0]);
   ASSERT_EQ("sky", words[1]);
 }
+
+TEST_F(LineToWordsClassTest, CanSplitLineUsingDynamicWordDelimiter)
+{
+	//Setup
+	char wordDelimiter = '#';
+	LineToWordsClass lineToWordsClass(wordDelimiter);
+	std::vector<std::string> words;
+
+	//Exercise
+	words = lineToWordsClass.splitLine("blue#red#green");
+
+	//Verify
+	ASSERT_EQ(3, words.size());
+	ASSERT_EQ("blue", words[0]);
+	ASSERT_EQ("red", words[1]);
+	ASSERT_EQ("green", words[2]);
+}
