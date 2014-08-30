06fef49fa6e6046d1be235b2755f277525d4118c
tdd1 <tdd1@tdd1-VirtualBox>
2014-08-30 21:06:16 +0200

    Create reference project for section 11 - Concatenate words into lines - Before refactoring concatenateWords

---
+++ reference/section10/WordsrepApp/src/WordsToLineClass.h
@@ -0,0 +1,13 @@
+#ifndef WORDSTOLINECLASS_H_
+#define WORDSTOLINECLASS_H_
+
+#include <string>
+#include <vector>
+
+class WordsToLineClass {
+public:
+	std::string concatenateWords(std::vector<std::string> words);
+};
+
+
+#endif /* WORDSTOLINECLASS_H_ */
