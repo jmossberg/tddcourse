0af622741612cca1c1a1e0050ffaaaee4b8b6957
tdd1 <tdd1@tdd1-VirtualBox>
2014-09-02 21:24:57 +0200

    Imported section 13 reference project into section 14 folder to use as baseline

---
+++ reference/section14/WordsrepApp/src/WordsrepClass.h
@@ -0,0 +1,24 @@
+#ifndef WORDSREPCLASS_H_
+#define WORDSREPCLASS_H_
+
+#include "AbstractFileReaderInterface.h"
+#include "AbstractFileWriterInterface.h"
+
+#include <string>
+#include <vector>
+
+class WordsrepClass {
+public:
+	std::vector<std::string> replaceMatchingWords(
+	    std::string oldWord, std::string newWord,
+	    std::vector<std::string> words);
+	void processInputFile(int& argc, const char* argv[],
+			AbstractFileReaderInterface& fileReader,
+			AbstractFileWriterInterface& fileWriter);
+private:
+  void insertLineFeed(AbstractFileWriterInterface& fileWriter);
+  bool isFirstLine;
+};
+
+
+#endif /* WORDSREPCLASS_H_ */
