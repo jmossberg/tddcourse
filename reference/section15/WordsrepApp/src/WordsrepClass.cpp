55a3a0a96eb2e2477cb5c0010bd4395119b28505
tdd1 <tdd1@tdd1-VirtualBox>
2014-09-03 21:21:03 +0200

    [Reference project section 15] Use for range loop in WordsrepClass::replaceMatchingWords

--- reference/section15/WordsrepApp/src/WordsrepClass.cpp
+++ reference/section15/WordsrepApp/src/WordsrepClass.cpp
@@ -4,21 +4,20 @@
 #include "WordsToLineClass.h"
 
 std::vector<std::string> WordsrepClass::replaceMatchingWords(
-std::string oldWord, std::string newWord,
-std::vector<std::string> words) {
+std::string& oldWord, std::string& newWord,
+std::vector<std::string>& words) {
 
-  std::vector<std::string>::iterator it;
   std::vector<std::string> newWords;
 
-  for(it = words.begin(); it != words.end(); it++)
+  for(auto& word: words)
   {
-    if((*it) == oldWord)
+    if(word == oldWord)
     {
       newWords.push_back(newWord);
     }
     else
     {
-      newWords.push_back(*it);
+      newWords.push_back(word);
     }
   }
 
