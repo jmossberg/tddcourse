ee805778802d24098e762ea1ff6fad67d931077d
tdd1 <tdd1@tdd1-VirtualBox>
2014-09-08 17:11:08 +0200

    [reference projects] Add comments about stringstream into splitLine method

--- reference/section14/WordsrepApp/src/LineToWordsClass.cpp
+++ reference/section14/WordsrepApp/src/LineToWordsClass.cpp
@@ -9,12 +9,19 @@ std::vector<std::string> LineToWordsClass::splitLine(
 
   std::vector<std::string> words;
 
+  //The stringstream class is good at parsing text. We will use
+  //it to split up the line into words.
   std::stringstream ss;
 
   //Copy string to stringstream
   ss << textLine;
 
   //Loop through stringstream looking for space delimiters
+  //Extract characters from ss into tempbuff until either
+  //the next character equals space or we have reached
+  //MAX_WORD_LENGTH number of characters
+  //The while loop will finish when there are no more words
+  //found by getline
   while(ss.getline(tempbuff, MAX_WORD_LENGTH, ' '))
   {
   	words.push_back(tempbuff);
--- reference/section17/WordsrepApp/src/LineToWordsClass.cpp
+++ reference/section17/WordsrepApp/src/LineToWordsClass.cpp
@@ -10,23 +10,30 @@ void LineToWordsClass::setWordDelimiter(const char& newWordDelimiter) {
 }
 
 std::vector<std::string> LineToWordsClass::splitLine(
-		const std::string& textLine) {
+	const std::string& textLine) {
 
-	const int MAX_WORD_LENGTH { 20 };
-	char tempbuff[50];
-
-	std::vector<std::string> words;
-
-	std::stringstream ss;
-
-	//Copy string to stringstream
-	ss << textLine;
-
-	//Loop through stringstream looking for delimiter character
-	while(ss.getline(tempbuff, MAX_WORD_LENGTH, wordDelimiter))
-	{
-		words.push_back(tempbuff);
-	}
-
-	return words;
+  const int MAX_WORD_LENGTH { 20 };
+  char tempbuff[50];
+  
+  std::vector<std::string> words;
+  
+  //The stringstream class is good at parsing text. We will use
+  //it to split up the line into words.
+  std::stringstream ss;
+  
+  //Copy string to stringstream
+  ss << textLine;
+  
+  //Loop through stringstream looking for delimiter character
+  //Extract characters from ss into tempbuff until either
+  //the next character equals space or we have reached
+  //MAX_WORD_LENGTH number of characters
+  //The while loop will finish when there are no more words
+  //found by getline
+  while(ss.getline(tempbuff, MAX_WORD_LENGTH, wordDelimiter))
+  {
+  	words.push_back(tempbuff);
+  }
+  
+  return words;
 }
